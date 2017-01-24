a2db0ae2dc97ed6b8ba79631d49a2499c30d7c25
ulrichvalverde <vanulrich.valverde@benilde.edu.ph>
2017-01-24 12:10:23 +0800

    Adjusted Sizes

--- register.php
+++ register.php
@@ -21,48 +21,49 @@
 	<body>
 		<div class="container">
 			<h1 class="text-center">Student Registration</h1>
-			<div class="col-lg-offset-3 col-lg-6">
+			<div class="col-lg-offset-4 col-lg-4">
 				<div class="well">
 					<form method="GET" action="welcome.php"
 						class="form-horizontal">
 
 						<div class="form-group">
-							<div class="col-lg-8">
+							<div class="col-lg-12">
 								<input type="text" name="sid"
 									class="form-control" placeholder="ID Number" required />
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-lg-8">
-								<input type="text" name="ln"
-									class="form-control" placeholder="Last Name" required />
-							</div>
-						</div>
-
-						<div class="form-group">
-							<div class="col-lg-8">
+							<div class="col-lg-12">
 								<input type="text" name="fn"
 									class="form-control" placeholder="First Name" required />
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-lg-8">
+							<div class="col-lg-12">
+								<input type="text" name="ln"
+									class="form-control" placeholder="Last Name" required />
+							</div>
+						</div>
+
+
+						<div class="form-group">
+							<div class="col-lg-12">
 								<input type="email" name="email"
 									class="form-control" placeholder="Email Address" required />
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-lg-8">
+							<div class="col-lg-12">
 								<input type="password" name="pwd"
 									class="form-control" placeholder="Password" required />
 							</div>
 						</div>
 
 						<div class="form-group">
-							<div class="col-lg-8">
+							<div class="col-lg-12">
 								<input type="date" name="bday"
 									class="form-control" required />
 							</div>
